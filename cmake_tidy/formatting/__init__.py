d7c55bc0cc9391e3df9e6c9285001fc3ed116fb6
Maciej Patro <maciejpatro@gmail.com>
2019-10-07 21:35:12 +0200

    loading settings from real file introduced and manually tested - do I need an automated test for it?

--- cmake_tidy/formatting/__init__.py
+++ cmake_tidy/formatting/__init__.py
@@ -1,7 +1,16 @@
+import json
+from pathlib import Path
+
 from cmake_tidy.formatting.cmake_formatter import CMakeFormatter
 
 
 def load_format_settings() -> dict:
+    settings_file = Path.cwd() / '.cmake-tidy.json'
+
+    if settings_file.exists():
+        with settings_file.open() as file:
+            return json.load(file)
+
     return _get_default_format_settings()
 
 

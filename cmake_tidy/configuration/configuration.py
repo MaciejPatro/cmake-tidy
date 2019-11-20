c119a620c5fdd5c5feba31d7a9b552c521046eb0
Maciej Patro <maciejpatro@gmail.com>
2019-11-20 20:54:26 +0100

    small function extractions

--- cmake_tidy/configuration/configuration.py
+++ cmake_tidy/configuration/configuration.py
@@ -14,10 +14,7 @@ class Configuration:
         input_file = self._config.get(self._property_name())
         if input_file is None:
             return ''
-        elif self.__input_data is None:
-            self.__input_data = input_file.read()
-            input_file.close()
-        return self.__input_data
+        return self.__get_input_data_from_file(input_file)
 
     @property
     def all_properties(self):
@@ -26,6 +23,12 @@ class Configuration:
     @staticmethod
     def _property_name():
         return inspect.stack()[1][3]
+
+    def __get_input_data_from_file(self, input_file):
+        if self.__input_data is None:
+            self.__input_data = input_file.read()
+            input_file.close()
+        return self.__input_data
 
     def __get_all_property_names(self) -> list:
         properties = [name for name, func in inspect.getmembers(self.__class__, self.__is_property)]
--- cmake_tidy/formatting/__init__.py
+++ cmake_tidy/formatting/__init__.py
@@ -6,12 +6,14 @@ from cmake_tidy.formatting.cmake_formatter import CMakeFormatter
 
 def load_format_settings() -> dict:
     settings_file = Path.cwd() / '.cmake-tidy.json'
-
     if settings_file.exists():
-        with settings_file.open() as file:
-            return json.load(file)
-
+        return _read_settings(settings_file)
     return _get_default_format_settings()
+
+
+def _read_settings(settings_file):
+    with settings_file.open() as file:
+        return json.load(file)
 
 
 def _get_default_format_settings() -> dict:

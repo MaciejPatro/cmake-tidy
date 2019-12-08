d8f6bb5d17335032903307254fdade168041a58c
Maciej Patro <maciejpatro@gmail.com>
2019-12-08 13:59:57 +0100

    BLUE: Improve implementation of newlines formatting

--- cmake_tidy/formatting/cmake_formatter.py
+++ cmake_tidy/formatting/cmake_formatter.py
@@ -37,17 +37,21 @@ class CMakeFormatter:
 
         formatting_methods['start_cmd_invoke'] = self.__format_start_of_command_invocation
         formatting_methods['spaces'] = lambda data: data.replace('\t', ' ' * self.__settings['tab_size'])
-        formatting_methods['newlines'] = self.__format_newlines
+        formatting_methods['newlines'] = self.__process_newlines
         formatting_methods['line_ending'] = lambda data: ''.join(data)
         formatting_methods['quoted_argument'] = lambda data: f'"{data}"'
         return formatting_methods
 
-    def __format_newlines(self, number_of_newlines: int) -> str:
-        formatted_newlines = '\n' * min(self.__settings['succeeding_newlines'], number_of_newlines)
-        newline_indentation = self.__state['indent'] * self.__settings['tab_size'] * ' '
-        return formatted_newlines + newline_indentation
+    def __process_newlines(self, number_of_newlines: int) -> str:
+        return self.__format_newlines(number_of_newlines) + self.__prepare_initial_newline_indent()
+
+    def __prepare_initial_newline_indent(self):
+        return self.__state['indent'] * self.__settings['tab_size'] * ' '
+
+    def __format_newlines(self, number_of_newlines):
+        return '\n' * min(self.__settings['succeeding_newlines'], number_of_newlines)
 
     def __format_start_of_command_invocation(self, data: str) -> str:
         if data.startswith('function'):
-            self.__state['indent'] = self.__state['indent'] + 1
+            self.__state['indent'] += 1
         return data

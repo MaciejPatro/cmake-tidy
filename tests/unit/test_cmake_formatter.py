a8c2de09b9542d20fd3540705832b7c7400f8547
Maciej Patro <maciejpatro@gmail.com>
2019-12-08 12:30:40 +0100

    Refactor unit tests for CMakeFormatter

--- tests/unit/test_cmake_formatter.py
+++ tests/unit/test_cmake_formatter.py
@@ -1,7 +1,7 @@
 import unittest
 
 from cmake_tidy.formatting import CMakeFormatter
-from tests.unit.parser_composite_elements import spaces, newlines
+from tests.unit.parser_composite_elements import spaces, newlines, command_invocation
 
 
 class TestCMakeFormatter(unittest.TestCase):
@@ -9,20 +9,28 @@ class TestCMakeFormatter(unittest.TestCase):
         self.__settings = {'succeeding_newlines': 1, 'tab_size': 2}
 
     def test_return_single_newline(self):
-        f = CMakeFormatter(self.__settings)
-        self.assertEqual('\n', f.format(newlines(3)))
+        self.assert_formatting(newlines(3), '\n')
 
     def test_return_3_newlines_although_settings_allow_more(self):
         self.__settings['succeeding_newlines'] = 5
-        f = CMakeFormatter(self.__settings)
-        self.assertEqual('\n\n\n', f.format(newlines(3)))
+        self.assert_formatting(newlines(3), '\n\n\n')
 
     def test_replace_tab_with_space_one_to_two(self):
-        f = CMakeFormatter(self.__settings)
-        self.assertEqual(' ' * 4, f.format(spaces('\t\t')))
+        self.assert_formatting(spaces('\t\t'), ' ' * 4)
 
     def test_replace_tabs_with_multiple_spaces(self):
         self.__settings['tab_size'] = 4
-        f = CMakeFormatter(self.__settings)
+        self.assert_formatting(spaces(' \t \t'), ' ' * 10)
 
-        self.assertEqual(' ' * 10, f.format(spaces(' \t \t')))
+    @unittest.SkipTest
+    def test_function_should_force_indentation_for_next_lines(self):
+        function_with_invocation_in_second_line = command_invocation('function(') \
+            .add(newlines(1)) \
+            .add(command_invocation('test('))
+        expected_formatting = 'function()\n  test()'
+
+        f = CMakeFormatter(self.__settings)
+        self.assertEqual(expected_formatting, f.format(function_with_invocation_in_second_line))
+
+    def assert_formatting(self, lex_data, formatted_string):
+        self.assertEqual(formatted_string, CMakeFormatter(self.__settings).format(lex_data))

5353017fa448bf9337ac363f9d20feb81dace660
Maciej Patro <maciejpatro@gmail.com>
2019-12-14 14:56:22 +0100

    GREEN - CMakeFormatDispatcher development now it can correctly handle state after dispatch execution

--- cmake_tidy/formatting/cmake_format_dispatcher.py
+++ cmake_tidy/formatting/cmake_format_dispatcher.py
@@ -4,4 +4,14 @@ class CMakeFormatDispatcher:
         self.__dict = {}
 
     def __setitem__(self, key, value):
+        if not callable(value):
+            raise TypeError('Only callable values accepted')
         self.__dict[key] = value
+
+    def __getitem__(self, item):
+        return self.__exec(item)
+
+    def __exec(self, item):
+        value = self.__dict[item]()
+        self.__state['last'] = item
+        return value
--- tests/unit/test_cmake_format_dispatcher.py
+++ tests/unit/test_cmake_format_dispatcher.py
@@ -5,8 +5,29 @@ from cmake_tidy.formatting.cmake_format_dispatcher import CMakeFormatDispatcher
 
 class TestCMakeFormatDispatcher(unittest.TestCase):
     def setUp(self) -> None:
-        self.state = {}
+        self.state = {'last': None}
         self.dispatcher = CMakeFormatDispatcher(self.state)
 
-    def test_should_allow_to_setitem_for_dispatching(self):
-        self.dispatcher['new'] = 1
+    def assertLastStateEqual(self, value):
+        self.assertEqual(self.state['last'], value)
+
+    def test_state_should_not_be_changed_when_object_created(self):
+        self.assertLastStateEqual(None)
+
+    def test_dispatched_method_should_remember_last_usage(self):
+        self.dispatcher['new'] = lambda: 1
+        self.assertEqual(1, self.dispatcher['new'])
+        self.assertLastStateEqual('new')
+
+    def test_ensure_that_state_changes_at_the_end_of_invocation(self):
+        self.dispatcher['some'] = lambda: self.state['last']
+        self.assertEqual(None, self.dispatcher['some'])
+        self.assertLastStateEqual('some')
+
+    def test_dispatcher_should_accept_only_callable_values(self):
+        with self.assertRaises(TypeError):
+            self.dispatcher['value'] = 1
+
+    def test_dispatcher_should_raise_key_error_when_no_such_key(self):
+        with self.assertRaises(KeyError):
+            self.dispatcher['x']

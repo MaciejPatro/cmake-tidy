1c3153aa6254fb02b13ad7561bd7b6aaa994aa31
Maciej Patro <maciejpatro@gmail.com>
2019-12-14 19:47:11 +0100

    BLUE: improvements in FormatStartCommandInvocation and in FormatFile

--- cmake_tidy/formatting/format_utils.py
+++ cmake_tidy/formatting/format_utils.py
@@ -16,22 +16,35 @@ class FormatNewline:
 class FormatStartCommandInvocation:
     def __init__(self, state: dict):
         self.__state = state
+        self.__invocations_with_indent = ['function', 'if']
 
     def __call__(self, data) -> str:
-        if data.startswith('function'):
-            self.__state['indent'] += 1
-        elif data.startswith('endfunction'):
-            self.__state['indent'] -= 1
+        self.__update_indent(data)
         return data
+
+    def __update_indent(self, data: str) -> None:
+        if data.startswith(self.__start_indents()):
+            self.__state['indent'] += 1
+        elif data.startswith(self.__end_indents()):
+            self.__state['indent'] -= 1
+
+    def __start_indents(self) -> tuple:
+        return tuple(self.__invocations_with_indent)
+
+    def __end_indents(self) -> tuple:
+        return tuple(f'end{x}' for x in self.__invocations_with_indent)
 
 
 class FormatFile:
     def __init__(self, settings: dict):
         self.__settings = settings
+        self.__elements_to_ident_backward = ['endfunction', 'endif']
 
     def __call__(self, data) -> str:
         return self.__cleanup_end_invocations(''.join(data))
 
     def __cleanup_end_invocations(self, formatted_file: str) -> str:
         indent = self.__settings['tab_size'] * ' '
-        return formatted_file.replace(indent + 'endfunction', 'endfunction')
+        for element in self.__elements_to_ident_backward:
+            formatted_file = formatted_file.replace(indent + element, element)
+        return formatted_file

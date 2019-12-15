a4aa7756165cc382de170bae267e70453e82e40e
Maciej Patro <maciejpatro@gmail.com>
2019-12-15 10:30:06 +0100

    RED: distance between arguments should be a single space if they are within a single line

--- tests/unit/test_cmake_formatter_arguments.py
+++ tests/unit/test_cmake_formatter_arguments.py
@@ -1,14 +1,28 @@
 from tests.unit.parser_composite_elements import spaces, file, command_invocation, unquoted_argument, \
-    arguments
+    arguments, quoted_argument
 from tests.unit.test_cmake_formatter import TestCMakeFormatter
 
 
 class TestCMakeFormatterCommandArguments(TestCMakeFormatter):
+    def assertFormattingArguments(self, expected_formatting, function_arguments):
+        self.assertFormatting(expected_formatting, file().add(command_invocation('function(', function_arguments)))
+
     def test_no_spaces_after_opening_and_before_closing_bracket(self):
         function_arguments = arguments() \
             .add(spaces('    ')) \
             .add(unquoted_argument('NAME')) \
             .add(spaces(' '))
-        function_with_invocation_in_second_line = file().add(command_invocation('function(', function_arguments))
         expected_formatting = 'function(NAME)'
-        self.assertFormatting(expected_formatting, function_with_invocation_in_second_line)
+        self.assertFormattingArguments(expected_formatting, function_arguments)
+
+    def test_trimming_spaces_between_arguments(self):
+        text = 'a text with \t tab    and multiple spaces'
+        function_arguments = arguments() \
+            .add(spaces('    ')) \
+            .add(unquoted_argument('NAME')) \
+            .add(spaces('    ')) \
+            .add(quoted_argument(text)) \
+            .add(spaces(' '))
+
+        expected_formatting = f'function(NAME \"{text}\")'
+        self.assertFormattingArguments(expected_formatting, function_arguments)

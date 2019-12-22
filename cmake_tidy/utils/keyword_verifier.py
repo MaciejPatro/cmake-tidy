822d81d6bab4c74760e6f6f992bb94b16c2cb377
Maciej Patro <maciej.patro@gmail.com>
2019-12-22 17:32:56 +0100

    GREEN: KeywordVerifier introduced - class checking whether string should be considered as keyword in preparation for different configurations for settings as keywords

---
+++ cmake_tidy/utils/keyword_verifier.py
@@ -0,0 +1,15 @@
+import re
+
+
+class KeywordVerifier:
+    def __init__(self, settings: dict):
+        self.__settings = settings
+
+    def is_keyword(self, data: str) -> bool:
+        return self.__is_one_of_defined_keywords(data) or self.__should_be_handled_as_keyword(data)
+
+    def __is_one_of_defined_keywords(self, data: str) -> bool:
+        return self.__settings.get('keywords') and data in self.__settings.get('keywords')
+
+    def __should_be_handled_as_keyword(self, data: str) -> bool:
+        return self.__settings.get('unquoted_uppercase_as_keyword') and re.match(r'^[A-Z]+$', data)
---
+++ tests/unit/test_keyword_verifier.py
@@ -0,0 +1,29 @@
+import unittest
+
+from cmake_tidy.utils.keyword_verifier import KeywordVerifier
+
+
+class TestKeywordVerifier(unittest.TestCase):
+    def setUp(self) -> None:
+        self.settings = {'keywords': ['TARGET', 'some'], 'unquoted_uppercase_as_keyword': False}
+
+    def test_should_accept_keyword_when_on_the_list(self):
+        verify = KeywordVerifier(self.settings)
+
+        self.assertTrue(verify.is_keyword('TARGET'))
+        self.assertTrue(verify.is_keyword('some'))
+        self.assertFalse(verify.is_keyword('TARGET2'))
+        self.assertFalse(verify.is_keyword('${TARGET}'))
+
+    def test_no_keywords_should_only_return_false(self):
+        verify = KeywordVerifier({})
+        self.assertFalse(verify.is_keyword('TARGET'))
+
+    def test_unquoted_arguments_with_uppercase_letters_only_are_keywords(self):
+        self.settings['unquoted_uppercase_as_keyword'] = True
+        verify = KeywordVerifier(self.settings)
+
+        self.assertTrue(verify.is_keyword('TARGET'))
+        self.assertTrue(verify.is_keyword('OTHER'))
+        self.assertFalse(verify.is_keyword('WITH_SEPARATION'))
+        self.assertFalse(verify.is_keyword('"$OTHER"'))

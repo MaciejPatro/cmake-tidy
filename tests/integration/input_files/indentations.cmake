6797e3ce8a701c63d590f791d480d5a3a7772301
Maciej Patro <maciej.patro@gmail.com>
2019-12-22 19:35:15 +0100

    BLUE: cover scenario when arguments could be an empty list and is not parsed properly

--- tests/integration/input_files/indentations.cmake
+++ tests/integration/input_files/indentations.cmake
@@ -4,7 +4,7 @@ cmake_minimum_required(VERSION 3.10)
 
 function(doubleEach)
             foreach(ARG ${ARGN})    # Iterate over each argument
-            if(${ARG} MATCHES "\w+")
+            if(${ARG} MATCHES "\w+" "")
 math(EXPR N "${ARG} * 2")
 else()
         message("${N}") # Print N

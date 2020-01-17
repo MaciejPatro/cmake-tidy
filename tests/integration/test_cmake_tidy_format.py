43675ca17a4ac4446aa6ef9f027dce9352844c60
Maciej Patro <maciejpatro@gmail.com>
2020-01-17 21:21:57 +0100

    RED: introduce a test for -i option for inplace formatting of the provided file

--- tests/integration/test_cmake_tidy_format.py
+++ tests/integration/test_cmake_tidy_format.py
@@ -4,7 +4,7 @@ from approvaltests.approvals import verify
 from io import StringIO
 
 from tests.integration.test_integration_base import TestIntegrationBase
-from tests.integration.utils import execute_cmake_tidy, normalize
+from tests.integration.utils import execute_cmake_tidy, normalize, get_input_file
 
 
 class TestCMakeTidyFormat(TestIntegrationBase):
@@ -24,4 +24,10 @@ class TestCMakeTidyFormat(TestIntegrationBase):
     def test_format_should_dump_config_only_configuration_to_stdout_by_default(self, stdout):
         self.assertSuccess(execute_cmake_tidy(command='format', arguments=['--dump-config', 'dummy.txt']))
         normalized_output = normalize(stdout.getvalue())
+        verify(normalized_output, self.reporter)
+
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_format_inplace_simple_file(self, file):
+        self.assertSuccess(execute_cmake_tidy(command='format', arguments=['-i', get_input_file('arguments.cmake')]))
+        normalized_output = normalize(file.getvalue())
         verify(normalized_output, self.reporter)

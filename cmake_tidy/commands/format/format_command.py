a840b2602b7e197ee8556aa1405134d8bfa6471c
Maciej Patro <maciejpatro@gmail.com>
2020-01-17 20:40:10 +0100

    GREEN: dump-config now correctly dumps json file to stdout

--- cmake_tidy/commands/format/format_command.py
+++ cmake_tidy/commands/format/format_command.py
@@ -1,3 +1,4 @@
+import json
 import sys
 
 from cmake_tidy.commands.format import try_create_configuration, FormatConfiguration
@@ -9,22 +10,12 @@ from cmake_tidy.commands.command import Command
 from cmake_tidy.utils.exit_codes import ExitCodes
 
 
-class FormatCommand(Command):
-    def __init__(self, parser):
-        description = 'format file to align it to standard'
-        super().__init__(parser, 'format', description)
-
-        arguments.dump_config(self._command_parser)
-        arguments.input_data(self._command_parser)
-
-    def execute_command(self, args) -> int:
+class _ExecuteFormat:
+    def format(self, args) -> int:
         try:
             config = try_create_configuration(args)
         except ConfigurationError as error:
             return self.__handle_configuration_error(error)
-        return self.__execute(config)
-
-    def __execute(self, config: FormatConfiguration) -> int:
         return self.__format_file(config)
 
     def __format_file(self, config: FormatConfiguration) -> int:
@@ -46,3 +37,18 @@ class FormatCommand(Command):
     def __handle_configuration_error(error):
         print('cmake-tidy format: ' + str(error), file=sys.stderr)
         return ExitCodes.FAILURE
+
+
+class FormatCommand(Command):
+    def __init__(self, parser):
+        description = 'format file to align it to standard'
+        super().__init__(parser, 'format', description)
+
+        arguments.dump_config(self._command_parser)
+        arguments.input_data(self._command_parser)
+
+    def execute_command(self, args) -> int:
+        if args.dump_config:
+            print(json.dumps(load_format_settings(), indent=2))
+            return ExitCodes.SUCCESS
+        return _ExecuteFormat().format(args)

38d1a6544888bdc3767fbd7617420b6975f29ec4
Maciej Patro <maciejpatro@gmail.com>
2020-01-17 20:44:30 +0100

    BLUE: small refactoring for FormatCommand

--- cmake_tidy/commands/format/format_command.py
+++ cmake_tidy/commands/format/format_command.py
@@ -10,33 +10,34 @@ from cmake_tidy.commands.command import Command
 from cmake_tidy.utils.exit_codes import ExitCodes
 
 
-class _ExecuteFormat:
-    def format(self, args) -> int:
-        try:
-            config = try_create_configuration(args)
-        except ConfigurationError as error:
-            return self.__handle_configuration_error(error)
-        return self.__format_file(config)
-
-    def __format_file(self, config: FormatConfiguration) -> int:
-        parsed_input = self.__parse_input(config.input)
-        formatted_data = self.__format_input_data(parsed_input)
+def _format(args) -> int:
+    def __format_file(configuration: FormatConfiguration) -> int:
+        parsed_input = __parse_input(configuration.input)
+        formatted_data = __format_input_data(parsed_input)
         print(formatted_data)
         return ExitCodes.SUCCESS
 
-    @staticmethod
     def __format_input_data(parsed_input):
         format_settings = load_format_settings()
         return CMakeFormatter(format_settings).format(parsed_input)
 
-    @staticmethod
     def __parse_input(input_data: str):
         return CMakeParser().parse(input_data)
 
-    @staticmethod
-    def __handle_configuration_error(error):
-        print('cmake-tidy format: ' + str(error), file=sys.stderr)
+    def __handle_configuration_error(raised_error):
+        print('cmake-tidy format: ' + str(raised_error), file=sys.stderr)
         return ExitCodes.FAILURE
+
+    try:
+        config = try_create_configuration(args)
+    except ConfigurationError as error:
+        return __handle_configuration_error(error)
+    return __format_file(config)
+
+
+def _dump_config() -> int:
+    print(json.dumps(load_format_settings(), indent=2))
+    return ExitCodes.SUCCESS
 
 
 class FormatCommand(Command):
@@ -49,6 +50,5 @@ class FormatCommand(Command):
 
     def execute_command(self, args) -> int:
         if args.dump_config:
-            print(json.dumps(load_format_settings(), indent=2))
-            return ExitCodes.SUCCESS
-        return _ExecuteFormat().format(args)
+            return _dump_config()
+        return _format(args)

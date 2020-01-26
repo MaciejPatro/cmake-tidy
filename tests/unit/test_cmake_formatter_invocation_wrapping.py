7f6faf9d3fd31ec6ca8f9b134a0c769826a4f09f
Maciej Patro <maciejpatro@gmail.com>
2020-01-26 12:38:44 +0100

    GREEN: skiptest and introduce new one for ensuring that multiple arguments function invocation is correctly wrapped

--- tests/unit/test_cmake_formatter_invocation_wrapping.py
+++ tests/unit/test_cmake_formatter_invocation_wrapping.py
@@ -1,3 +1,5 @@
+import unittest
+
 from tests.unit.parser_composite_elements import arguments, newlines, spaces, unquoted_argument, file, \
     command_invocation
 from tests.unit.test_cmake_formatter import TestCMakeFormatter
@@ -9,13 +11,16 @@ class TestCMakeFormatterCommandInvocationsWrapping(TestCMakeFormatter):
         args = arguments() \
                 .add(newlines(4)) \
                 .add(spaces('    ')) \
-                .add(unquoted_argument('argument'))
+                .add(unquoted_argument('argument1')) \
+                .add(spaces('    ')) \
+                .add(unquoted_argument('argument2'))
         root = file().add(command_invocation('function_call(', args))
 
-        expected_formatting = """function_call(argument)"""
+        expected_formatting = """function_call(argument1 argument2)"""
 
         self.assertFormatting(expected_formatting, root)
 
+    @unittest.SkipTest
     def test_invocation_wrapping_only_when_line_length_is_smaller_than_set_threshold(self):
         self.settings['wrap_short_invocations_to_single_line'] = True
         self.settings['line_length'] = 15

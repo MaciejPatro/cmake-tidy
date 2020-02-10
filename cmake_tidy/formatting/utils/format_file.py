dcb4cb413d6911c1dd720e38caf5d5b150450b51
Maciej Patro <maciejpatro@gmail.com>
2020-02-10 20:19:04 +0100

    Add additional unit test for keyword case when identation changes - to ensure correct closing parenthesis identation

--- cmake_tidy/formatting/utils/format_file.py
+++ cmake_tidy/formatting/utils/format_file.py
@@ -11,8 +11,10 @@ class FormatFile:
 
     def __cleanup_end_invocations(self, formatted_file: str) -> str:
         indent = get_single_indent(self.__settings)
-        formatted_file = formatted_file.replace(2 * indent + Tokens.reindent_2, '')
-        formatted_file = formatted_file.replace(indent + Tokens.reindent, '')
-        formatted_file = formatted_file.replace(Tokens.reindent_2, '')
-        formatted_file = formatted_file.replace(Tokens.reindent, '')
+        for pattern in [2 * indent + Tokens.reindent_2,
+                        indent + Tokens.reindent_2,
+                        indent + Tokens.reindent,
+                        Tokens.reindent_2,
+                        Tokens.reindent]:
+            formatted_file = formatted_file.replace(pattern, '')
         return formatted_file

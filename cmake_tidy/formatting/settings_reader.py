5a69da0ae22f3f3f4103998d13b401c7fa003293
Maciej Patro <maciejpatro@gmail.com>
2020-02-19 21:52:03 +0100

    Move all the functions into the SettingsReader class - access to private members should most likely be refactored in tests

--- cmake_tidy/formatting/settings_reader.py
+++ cmake_tidy/formatting/settings_reader.py
@@ -2,36 +2,32 @@ import json
 from pathlib import Path
 
 
-def load_format_settings() -> dict:
-    settings = _get_default_format_settings()
-    settings.update(_read_settings())
-    return settings
-
-
-def _read_settings():
-    settings_file = Path.cwd() / '.cmake-tidy.json'
-    if settings_file.exists():
-        with settings_file.open() as file:
-            return json.load(file)
-    return dict()
-
-
-def _get_default_format_settings() -> dict:
-    settings = dict()
-    settings['succeeding_newlines'] = 2
-    settings['tabs_as_spaces'] = True
-    settings['tab_size'] = 4
-    settings['force_command_lowercase'] = True
-    settings['space_between_command_and_begin_parentheses'] = False
-    settings['line_length'] = 80
-    settings['wrap_short_invocations_to_single_line'] = False
-    settings['closing_parentheses_in_newline_when_split'] = False
-    settings['unquoted_uppercase_as_keyword'] = False
-    settings['keywords'] = []
-    return settings
-
-
 class SettingsReader:
     @staticmethod
     def load_format_settings() -> dict:
-        return load_format_settings()
+        settings = SettingsReader._get_default_format_settings()
+        settings.update(SettingsReader._read_settings())
+        return settings
+
+    @staticmethod
+    def _read_settings():
+        settings_file = Path.cwd() / '.cmake-tidy.json'
+        if settings_file.exists():
+            with settings_file.open() as file:
+                return json.load(file)
+        return dict()
+
+    @staticmethod
+    def _get_default_format_settings() -> dict:
+        settings = dict()
+        settings['succeeding_newlines'] = 2
+        settings['tabs_as_spaces'] = True
+        settings['tab_size'] = 4
+        settings['force_command_lowercase'] = True
+        settings['space_between_command_and_begin_parentheses'] = False
+        settings['line_length'] = 80
+        settings['wrap_short_invocations_to_single_line'] = False
+        settings['closing_parentheses_in_newline_when_split'] = False
+        settings['unquoted_uppercase_as_keyword'] = False
+        settings['keywords'] = []
+        return settings
--- tests/integration/test_file_formatting.py
+++ tests/integration/test_file_formatting.py
@@ -3,7 +3,7 @@ from unittest import mock
 from approvaltests.approvals import verify
 from io import StringIO
 
-from cmake_tidy.formatting.settings_reader import _get_default_format_settings
+from cmake_tidy.formatting import SettingsReader
 from tests.integration.test_integration_base import TestIntegrationBase
 from tests.integration.utils import execute_cmake_tidy, normalize, get_input_file
 
@@ -24,7 +24,7 @@ class TestFileFormatting(TestIntegrationBase):
     @mock.patch('sys.stdout', new_callable=StringIO)
     @mock.patch('cmake_tidy.commands.format.format_command.SettingsReader.load_format_settings')
     def test_format_against_newline_violations_with_custom_settings(self, load_settings, stdout):
-        fake_settings = _get_default_format_settings()
+        fake_settings = SettingsReader._get_default_format_settings()
         fake_settings['succeeding_newlines'] = 4
         load_settings.return_value = fake_settings
 
@@ -54,7 +54,7 @@ class TestFileFormatting(TestIntegrationBase):
     @mock.patch('sys.stdout', new_callable=StringIO)
     @mock.patch('cmake_tidy.commands.format.format_command.SettingsReader.load_format_settings')
     def test_format_indentation_when_spaces_after_command_name_are_present(self, load_settings, stdout):
-        fake_settings = _get_default_format_settings()
+        fake_settings = SettingsReader._get_default_format_settings()
         fake_settings['space_between_command_and_begin_parentheses'] = True
         load_settings.return_value = fake_settings
 
@@ -66,7 +66,7 @@ class TestFileFormatting(TestIntegrationBase):
     @mock.patch('sys.stdout', new_callable=StringIO)
     @mock.patch('cmake_tidy.commands.format.format_command.SettingsReader.load_format_settings')
     def test_format_line_splitting(self, load_settings, stdout):
-        fake_settings = _get_default_format_settings()
+        fake_settings = SettingsReader._get_default_format_settings()
         fake_settings['wrap_short_invocations_to_single_line'] = True
         load_settings.return_value = fake_settings
 
@@ -78,7 +78,7 @@ class TestFileFormatting(TestIntegrationBase):
     @mock.patch('sys.stdout', new_callable=StringIO)
     @mock.patch('cmake_tidy.commands.format.format_command.SettingsReader.load_format_settings')
     def test_formatting_with_tabs(self, load_settings, stdout):
-        fake_settings = _get_default_format_settings()
+        fake_settings = SettingsReader._get_default_format_settings()
         fake_settings['tabs_as_spaces'] = False
         load_settings.return_value = fake_settings
 

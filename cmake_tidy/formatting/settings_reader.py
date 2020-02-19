289b9727b086519af795d9a41715f228763313e0
Maciej Patro <maciejpatro@gmail.com>
2020-02-19 21:42:17 +0100

    Move functions for reading settings to separate file in preparation for combining them into class

---
+++ cmake_tidy/formatting/settings_reader.py
@@ -0,0 +1,31 @@
+import json
+from pathlib import Path
+
+
+def load_format_settings() -> dict:
+    settings = _get_default_format_settings()
+    settings.update(_read_settings())
+    return settings
+
+
+def _read_settings():
+    settings_file = Path.cwd() / '.cmake-tidy.json'
+    if settings_file.exists():
+        with settings_file.open() as file:
+            return json.load(file)
+    return dict()
+
+
+def _get_default_format_settings() -> dict:
+    settings = dict()
+    settings['succeeding_newlines'] = 2
+    settings['tabs_as_spaces'] = True
+    settings['tab_size'] = 4
+    settings['force_command_lowercase'] = True
+    settings['space_between_command_and_begin_parentheses'] = False
+    settings['line_length'] = 80
+    settings['wrap_short_invocations_to_single_line'] = False
+    settings['closing_parentheses_in_newline_when_split'] = False
+    settings['unquoted_uppercase_as_keyword'] = False
+    settings['keywords'] = []
+    return settings

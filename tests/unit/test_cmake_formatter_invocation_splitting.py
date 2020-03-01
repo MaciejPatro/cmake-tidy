596481732e97b0966160bb5b97f471a65c855ce0
Maciej Patro <maciejpatro@gmail.com>
2020-03-01 19:46:39 +0100

    Split unit tests for wrapping and splitting invocation to 2 separate files

---
+++ tests/unit/test_cmake_formatter_invocation_splitting.py
@@ -0,0 +1,66 @@
+from tests.unit.parser_composite_elements import arguments, spaces, unquoted_argument, file, command_invocation
+from tests.unit.test_cmake_formatter import TestCMakeFormatter
+
+
+class TestCMakeFormatterCommandInvocationSplitting(TestCMakeFormatter):
+    def test_invocation_splitting_when_line_length_exceeded(self):
+        self.settings['line_length'] = 15
+        args = arguments().add(unquoted_argument('abc')) \
+            .add(spaces('    ')) \
+            .add(unquoted_argument('def'))
+        root = file().add(command_invocation('a_very_long_name(', args))
+
+        expected_formatting = """a_very_long_name(abc
+  def)"""
+        self.assertFormatting(expected_formatting, root)
+
+    def test_invocation_splitting_with_closing_parentheses_in_newline(self):
+        self.settings['line_length'] = 15
+        self.settings['closing_parentheses_in_newline_when_split'] = True
+        args = arguments().add(unquoted_argument('abc')) \
+            .add(spaces('    ')) \
+            .add(unquoted_argument('def'))
+        root = file().add(command_invocation('a_very_long_name(', args))
+
+        expected_formatting = """a_very_long_name(abc
+  def
+)"""
+        self.assertFormatting(expected_formatting, root)
+
+    def test_invocation_splitting_with_closing_parentheses_in_newline_and_keyword(self):
+        self.settings['line_length'] = 15
+        self.settings['closing_parentheses_in_newline_when_split'] = True
+        args = arguments().add(unquoted_argument('abc')) \
+            .add(spaces('    ')) \
+            .add(unquoted_argument('TARGET')) \
+            .add(spaces('    ')) \
+            .add(unquoted_argument('def'))
+        root = file().add(command_invocation('a_very_long_name(', args))
+
+        expected_formatting = """a_very_long_name(abc
+  TARGET
+    def
+)"""
+        self.assertFormatting(expected_formatting, root)
+
+    def test_invocation_splitting_with_keywords_inside(self):
+        self.settings['line_length'] = 5
+        args = arguments().add(unquoted_argument('abc')) \
+            .add(spaces('    ')) \
+            .add(unquoted_argument('TARGET')) \
+            .add(spaces('    ')) \
+            .add(unquoted_argument('def')) \
+            .add(spaces('    ')) \
+            .add(unquoted_argument('TARGET')) \
+            .add(spaces('    ')) \
+            .add(unquoted_argument('def'))
+
+        root = file().add(command_invocation('function(', args))
+
+        expected_formatting = """function(abc
+  TARGET
+    def
+  TARGET
+    def)"""
+
+        self.assertFormatting(expected_formatting, root)

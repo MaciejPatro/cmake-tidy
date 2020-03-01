f53e498f523dc5f75d188489ed11ec84ca528e6a
Maciej Patro <maciejpatro@gmail.com>
2020-03-01 11:03:12 +0100

    Fix #14 - spacing between command invocation and line comment is now normalized

--- cmake_tidy/formatting/utils/format_spaces.py
+++ cmake_tidy/formatting/utils/format_spaces.py
@@ -9,4 +9,6 @@ class FormatSpaces:
     def __call__(self, data: str) -> str:
         if self.__state['last'] == 'line_ending':
             return ''
+        elif self.__state['last'] == 'command_invocation':
+            return ' '
         return data.replace('\t', get_single_indent(self.__settings))
--- tests/unit/test_cmake_formatter_elements_interactions.py
+++ tests/unit/test_cmake_formatter_elements_interactions.py
@@ -1,11 +1,11 @@
-from tests.unit.parser_composite_elements import file, command_invocation, spaces, line_ending
 from tests.unit.test_cmake_formatter import TestCMakeFormatter
+from tests.unit.parser_composite_elements import file, command_invocation, spaces, line_ending
 
 
 class TestCMakeFormatterElementsInteractions(TestCMakeFormatter):
     def test_command_invocation_should_be_by_default_lowercase(self):
         invocation = file() \
-            .add(command_invocation('FUNCTION(')) \
+            .add(command_invocation('abc(')) \
             .add(spaces('   \t')) \
             .add(line_ending('# a comment', 1))
-        self.assertFormatting('function() # a comment', invocation)
+        self.assertFormatting('abc() # a comment\n', invocation)

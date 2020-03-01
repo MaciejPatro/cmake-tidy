d7313ad74be1300eb53fa6a207798842a03cfe5a
Maciej Patro <maciejpatro@gmail.com>
2020-03-01 20:39:53 +0100

    Fix #8, Fix #5 splitting invocation with line comments should work correctly now

--- cmake_tidy/formatting/utils/invocation_splitter.py
+++ cmake_tidy/formatting/utils/invocation_splitter.py
@@ -10,16 +10,38 @@ class InvocationSplitter:
 
     def split(self, invocation: dict) -> list:
         initial_indent = self.__state['indent']
+        arguments = self.__prepare_newline_split_list_of_arguments(initial_indent, invocation)
+        self.__ensure_closing_bracket_position(arguments)
+        self.__rollback_indent_state(initial_indent)
+        return arguments
+
+    def __prepare_newline_split_list_of_arguments(self, initial_indent, invocation):
         arguments = []
         for arg in invocation['arguments']:
             newline = FormatNewline(self.__state, self.__settings)(1)
             if arg == ' ':
                 arguments.append(newline)
             else:
-                if self.__verifier.is_keyword(arg):
-                    self.__state['indent'] = initial_indent + 1
-                arguments.append(arg)
+                self.__handle_non_whitespace_arguments(arg, arguments, initial_indent)
+        return arguments
+
+    def __rollback_indent_state(self, initial_indent):
+        self.__state['indent'] = initial_indent
+
+    def __ensure_closing_bracket_position(self, arguments):
         if self.__settings['closing_parentheses_in_newline_when_split']:
             arguments.append(FormatNewline(self.__state, self.__settings)(1))
-        self.__state['indent'] = initial_indent
-        return arguments
+
+    def __handle_non_whitespace_arguments(self, arg, arguments, initial_indent):
+        self.__update_indent_state(arg, initial_indent)
+        self.__fix_line_comment(arg, arguments)
+        arguments.append(arg)
+
+    @staticmethod
+    def __fix_line_comment(current_argument, arguments):
+        if current_argument.startswith('#'):
+            arguments[-1] = ' '
+
+    def __update_indent_state(self, arg, initial_indent):
+        if self.__verifier.is_keyword(arg):
+            self.__state['indent'] = initial_indent + 1

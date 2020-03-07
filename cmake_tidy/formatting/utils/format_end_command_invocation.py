325787a3257d2dccdd1bcb0c6738b772b2596bfb
Maciej Patro <maciejpatro@gmail.com>
2020-03-07 20:31:57 +0100

    Final cleanup - renaming. Fix #7, Fix #32 finished

--- cmake_tidy/formatting/utils/format_end_command_invocation.py
+++ cmake_tidy/formatting/utils/format_end_command_invocation.py
@@ -6,7 +6,7 @@ class FormatEndCommandInvocation:
         self.__state = state
 
     def __call__(self, data: str) -> str:
-        if self.__state['has_properties_keyword']:
+        if self.__state['has_first_class_keyword']:
             return Tokens.reindent(3) + data
         if self.__state['keyword_argument']:
             return Tokens.reindent(2) + data

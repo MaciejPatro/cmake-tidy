db507444dd7f53b74080bf7e57e940c7fec628e0
Maciej Patro <maciejpatro@gmail.com>
2020-03-14 21:26:05 +0100

    Move tokens responsibility to Tokens class

--- cmake_tidy/formatting/utils/tokens.py
+++ cmake_tidy/formatting/utils/tokens.py
@@ -1,5 +1,17 @@
 class Tokens:
     @staticmethod
+    def start_tokens() -> list:
+        return ['macro', 'while', 'foreach', 'if', 'function']
+
+    @staticmethod
+    def end_tokens() -> list:
+        return [f'end{token}' for token in Tokens.start_tokens()]
+
+    @staticmethod
+    def reindent_commands_tokens() -> list:
+        return ['elseif', 'else'] + Tokens.end_tokens()
+
+    @staticmethod
     def reindent(count: int) -> str:
         return f'<cmake-tidy-reindent{count}>'
 

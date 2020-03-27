91dabed141f96ed332978b2e1075cd6f6ccd5981
Maciej Patro <maciejpatro@gmail.com>
2020-03-27 19:11:04 +0100

    State updater extraction from FormatCommandInvocation as the class starts to have too big complexity

---
+++ cmake_tidy/formatting/utils/command_invocatin_state_updater.py
@@ -0,0 +1,31 @@
+import re
+
+from cmake_tidy.formatting.utils.tokens import Tokens
+
+
+class CommandInvocationStateUpdater:
+    def __init__(self, state: dict):
+        self.__state = state
+
+    def update_state(self, function_name: str) -> None:
+        self.__update_indent_state(function_name)
+        self.__state['keyword_argument'] = False
+        self.__state['has_first_class_keyword'] = False
+
+    def __update_indent_state(self, function_name: str) -> None:
+        if not self.__is_start_of_special_command(function_name):
+            self.__state['indent'] -= 1
+        if self.__state['has_first_class_keyword']:
+            self.__state['indent'] -= 1
+        if self.__is_end_of_special_command(function_name):
+            self.__state['indent'] -= 1
+
+    def __is_start_of_special_command(self, original: str) -> bool:
+        return any([self.__matches(token, original) for token in Tokens.start_tokens()])
+
+    def __is_end_of_special_command(self, original: str) -> bool:
+        return any([self.__matches(token, original) for token in Tokens.end_tokens()])
+
+    @staticmethod
+    def __matches(token: str, data: str) -> bool:
+        return re.match(r'^' + re.escape(token) + r'\s?\(', data) is not None

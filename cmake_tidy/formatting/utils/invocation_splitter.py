fc78c4c21100d96ea37a5ae0af3b9e6780cf0e2a
Maciej Patro <maciejpatro@gmail.com>
2020-03-28 15:43:11 +0100

    InvocationSplitter refactoring ongoing

--- cmake_tidy/formatting/utils/invocation_splitter.py
+++ cmake_tidy/formatting/utils/invocation_splitter.py
@@ -9,43 +9,46 @@ from cmake_tidy.utils.keyword_verifier import KeywordVerifier
 class InvocationSplitter:
     def __init__(self, state: dict, settings: dict):
         self.__state = state
+        self.__initial_indent = self.__state['indent']
         self.__settings = settings
         self.__verifier = KeywordVerifier(settings)
 
     def split(self, invocation: dict) -> list:
-        initial_indent = self.__state['indent']
-        arguments = self.__prepare_newline_split_list_of_arguments(initial_indent, invocation)
-        self.__ensure_closing_bracket_position(arguments)
-        self.__rollback_indent_state(initial_indent)
+        self.__initial_indent = self.__state['indent']
+        arguments = self.__split_args_to_newlines(invocation) + self.__add_closing_bracket_separator()
+        self.__rollback_indent_state()
         return arguments
 
-    def __prepare_newline_split_list_of_arguments(self, initial_indent, invocation):
+    def __split_args_to_newlines(self, invocation: dict) -> list:
         arguments = []
         for arg in invocation['arguments']:
-            newline = FormatNewline(self.__state, self.__settings)(1)
             if arg == ' ':
-                arguments.append(newline)
+                arguments += self.__get_converted_whitespace()
             else:
-                self.__handle_non_whitespace_arguments(arg, arguments, initial_indent)
+                self.__handle_non_whitespace_arguments(arg, arguments)
         return arguments
 
-    def __rollback_indent_state(self, initial_indent):
-        self.__state['indent'] = initial_indent
+    def __get_converted_whitespace(self) -> list:
+        return [(FormatNewline(self.__state, self.__settings)(1))]
 
-    def __ensure_closing_bracket_position(self, arguments):
+    def __rollback_indent_state(self) -> None:
+        self.__state['indent'] = self.__initial_indent
+
+    def __add_closing_bracket_separator(self) -> list:
         if self.__settings['closing_parentheses_in_newline_when_split']:
-            arguments.append(FormatNewline(self.__state, self.__settings)(1))
+            return [FormatNewline(self.__state, self.__settings)(1)]
+        return []
 
-    def __handle_non_whitespace_arguments(self, arg, arguments, initial_indent):
-        self.__update_indent_state(arg, initial_indent)
+    def __handle_non_whitespace_arguments(self, arg: str, arguments: list) -> None:
+        self.__update_indent_state(arg)
         self.__fix_line_comment(arg, arguments)
         arguments.append(arg)
+
+    def __update_indent_state(self, arg: str):
+        if self.__verifier.is_keyword(arg):
+            self.__state['indent'] = self.__initial_indent + 1
 
     @staticmethod
     def __fix_line_comment(current_argument, arguments):
         if current_argument.startswith('#'):
             arguments[-1] = ' '
-
-    def __update_indent_state(self, arg, initial_indent):
-        if self.__verifier.is_keyword(arg):
-            self.__state['indent'] = initial_indent + 1

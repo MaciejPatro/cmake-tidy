70c0feef50dfb8a224553b8f0e556561c76950d9
Maciej Patro <maciejpatro@gmail.com>
2020-03-29 11:41:19 +0200

    Refactoring introduced - now Keyword state is correctly handled by one class and there is no copy-paste of functionality

--- cmake_tidy/formatting/utils/format_unquoted_argument.py
+++ cmake_tidy/formatting/utils/format_unquoted_argument.py
@@ -2,6 +2,7 @@
 # Copyright Maciej Patro (maciej.patro@gmail.com)
 # MIT License
 ###############################################################################
+from cmake_tidy.formatting.utils.keyword_state_updater import KeywordStateUpdater
 from cmake_tidy.formatting.utils.tokens import Tokens
 from cmake_tidy.utils.keyword_verifier import KeywordVerifier
 
@@ -9,22 +10,14 @@ from cmake_tidy.utils.keyword_verifier import KeywordVerifier
 class FormatUnquotedArgument:
     def __init__(self, state: dict, settings: dict):
         self.__verifier = KeywordVerifier(settings)
+        self.__state_updater = KeywordStateUpdater(state, settings)
         self.__state = state
         self.__keyword_argument_already_found = False
 
     def __call__(self, data: str) -> str:
         self.__keyword_argument_already_found = self.__state['keyword_argument']
-        self.__update_state(data)
+        self.__state_updater.update_state(data)
         return self.__format_data(data)
-
-    def __update_state(self, data: str) -> None:
-        if self.__verifier.is_first_class_keyword(data):
-            self.__state['has_first_class_keyword'] = True
-            self.__state['indent'] += 1
-        elif self.__verifier.is_keyword(data):
-            if not self.__state['keyword_argument']:
-                self.__state['indent'] += 1
-            self.__state['keyword_argument'] = True
 
     def __format_data(self, data: str) -> str:
         if self.__keyword_argument_already_found and self.__verifier.is_keyword(data):
---
+++ cmake_tidy/formatting/utils/keyword_state_updater.py
@@ -0,0 +1,20 @@
+###############################################################################
+# Copyright Maciej Patro (maciej.patro@gmail.com)
+# MIT License
+###############################################################################
+from cmake_tidy.utils.keyword_verifier import KeywordVerifier
+
+
+class KeywordStateUpdater:
+    def __init__(self, state: dict, settings: dict):
+        self.__state = state
+        self.__verifier = KeywordVerifier(settings)
+
+    def update_state(self, argument: str) -> None:
+        if self.__verifier.is_first_class_keyword(argument):
+            self.__state['has_first_class_keyword'] = True
+            self.__state['indent'] += 1
+        elif self.__verifier.is_keyword(argument):
+            if not self.__state['keyword_argument']:
+                self.__state['indent'] += 1
+            self.__state['keyword_argument'] = True

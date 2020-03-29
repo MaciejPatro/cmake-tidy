5165cc4240f3bb80a84a64b8ec17ef811db556de
Maciej Patro <maciejpatro@gmail.com>
2020-03-29 10:59:34 +0200

    Improve handling of first class keywords

--- cmake_tidy/formatting/utils/invocation_splitter.py
+++ cmake_tidy/formatting/utils/invocation_splitter.py
@@ -9,6 +9,11 @@ from cmake_tidy.utils.keyword_verifier import KeywordVerifier
 class InvocationSplitter:
     def __init__(self, state: dict, settings: dict):
         self.__state = state.copy()
+        if self.__state['has_first_class_keyword']:
+            self.__state['indent'] -= 1
+        self.__state['has_first_class_keyword'] = False
+        self.__state['keyword_argument'] = False
+
         self.__settings = settings
         self.__verifier = KeywordVerifier(settings)
 
@@ -25,8 +30,13 @@ class InvocationSplitter:
         return self.__get_converted_whitespace() if arg == ' ' else arg
 
     def __update_indent_state(self, arg: str):
-        if self.__verifier.is_keyword(arg):
-            self.__state['indent'] = self.__state['indent'] + 1
+        if self.__verifier.is_first_class_keyword(arg):
+            self.__state['has_first_class_keyword'] = True
+            self.__state['indent'] += 1
+        elif self.__verifier.is_keyword(arg):
+            if not self.__state['keyword_argument']:
+                self.__state['indent'] += 1
+            self.__state['keyword_argument'] = True
 
     def __add_closing_bracket_separator(self) -> list:
         if self.__settings['closing_parentheses_in_newline_when_split']:

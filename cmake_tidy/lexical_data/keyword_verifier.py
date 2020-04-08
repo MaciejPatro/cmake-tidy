3d946337bdffbceff9fe609b8c6bc0b0d4f09f59
Maciej Patro <maciejpatro@gmail.com>
2020-04-08 19:18:09 +0200

    Ongoing extensions of KeywordVerifier - now it reads property file and is able to correctl check whether some part of unqouted parameters are really properties or not

--- cmake_tidy/lexical_data/keyword_verifier.py
+++ cmake_tidy/lexical_data/keyword_verifier.py
@@ -2,17 +2,25 @@
 # Copyright Maciej Patro (maciej.patro@gmail.com)
 # MIT License
 ###############################################################################
-
-
+import json
 import re
+from pathlib import Path
 
 from cmake_tidy.formatting.utils.tokens import Tokens
 
 
 class KeywordVerifier:
     __FIRST_CLASS_KEYWORDS = ['PROPERTIES', 'PROPERTY']
+    __PROPERTIES = dict()
+
+    @staticmethod
+    def __init_properties():
+        if not KeywordVerifier.__PROPERTIES:
+            with (Path(__file__).parent / 'property_list.json').open() as file:
+                KeywordVerifier.__PROPERTIES = json.load(file)
 
     def __init__(self, settings: dict):
+        KeywordVerifier.__init_properties()
         self.__settings = settings
 
     @staticmethod
@@ -20,14 +28,26 @@ class KeywordVerifier:
         data = data.replace(Tokens.reindent(1), '')
         return data in KeywordVerifier.__FIRST_CLASS_KEYWORDS
 
+    @staticmethod
+    def get_cmake_properties_version() -> str:
+        return KeywordVerifier.__PROPERTIES.get("cmake_version")
+
     def is_keyword(self, data: str) -> bool:
         data = data.replace(Tokens.reindent(1), '')
         return self.__is_one_of_defined_keywords(data) or \
                self.__should_be_handled_as_keyword(data) or \
                self.is_first_class_keyword(data)
 
+    def __should_be_handled_as_keyword(self, data: str) -> bool:
+        return self.__settings.get('unquoted_uppercase_as_keyword') and re.match(r'^[A-Z]+$', data)
+
     def __is_one_of_defined_keywords(self, data: str) -> bool:
         return self.__settings.get('keywords') and data in self.__settings.get('keywords')
 
-    def __should_be_handled_as_keyword(self, data: str) -> bool:
-        return self.__settings.get('unquoted_uppercase_as_keyword') and re.match(r'^[A-Z]+$', data)
+    def is_property(self, data: str) -> bool:
+        return data in self.__PROPERTIES.get("full_names") or \
+               self.__is_property_regex_starting(data)
+
+    @staticmethod
+    def __is_property_regex_starting(data):
+        return any([data.startswith(token) for token in KeywordVerifier.__PROPERTIES.get('starting_with')])
--- tests/unit/test_keyword_verifier.py
+++ tests/unit/test_keyword_verifier.py
@@ -5,31 +5,61 @@
 
 
 import unittest
+from unittest import mock
+from unittest.mock import MagicMock
 
+from cmake_tidy.formatting.utils.tokens import Tokens
 from cmake_tidy.lexical_data import KeywordVerifier
 
 
 class TestKeywordVerifier(unittest.TestCase):
     def setUp(self) -> None:
         self.settings = {'keywords': ['TARGET', 'some'], 'unquoted_uppercase_as_keyword': False}
+        self.verify = KeywordVerifier(self.settings)
 
     def test_should_accept_keyword_when_on_the_list(self):
-        verify = KeywordVerifier(self.settings)
-
-        self.assertTrue(verify.is_keyword('TARGET'))
-        self.assertTrue(verify.is_keyword('some'))
-        self.assertFalse(verify.is_keyword('TARGET2'))
-        self.assertFalse(verify.is_keyword('${TARGET}'))
+        self.assertTrue(self.verify.is_keyword('TARGET'))
+        self.assertTrue(self.verify.is_keyword('some'))
+        self.assertFalse(self.verify.is_keyword('TARGET2'))
+        self.assertFalse(self.verify.is_keyword('${TARGET}'))
 
     def test_no_keywords_should_only_return_false(self):
-        verify = KeywordVerifier({})
-        self.assertFalse(verify.is_keyword('TARGET'))
+        self.verify = KeywordVerifier({})
+        self.assertFalse(self.verify.is_keyword('TARGET'))
 
     def test_unquoted_arguments_with_uppercase_letters_only_are_keywords(self):
         self.settings['unquoted_uppercase_as_keyword'] = True
-        verify = KeywordVerifier(self.settings)
+        self.verify = KeywordVerifier(self.settings)
 
-        self.assertTrue(verify.is_keyword('TARGET'))
-        self.assertTrue(verify.is_keyword('OTHER'))
-        self.assertFalse(verify.is_keyword('WITH_SEPARATION'))
-        self.assertFalse(verify.is_keyword('"$OTHER"'))
+        self.assertTrue(self.verify.is_keyword('TARGET'))
+        self.assertTrue(self.verify.is_keyword('OTHER'))
+        self.assertTrue(self.verify.is_keyword(Tokens.reindent(1) + 'OTHER'))
+        self.assertFalse(self.verify.is_keyword('WITH_SEPARATION'))
+        self.assertFalse(self.verify.is_keyword('"$OTHER"'))
+
+    @mock.patch('builtins.open')
+    def test_ensure_properties_read_only_once(self, mock_open: MagicMock):
+        self.verify = KeywordVerifier(self.settings)
+        self.assertFalse(mock_open.called)
+
+    def test_whether_token_is_first_class_keyword(self):
+        self.assertTrue(self.verify.is_first_class_keyword('PROPERTY'))
+        self.assertTrue(self.verify.is_first_class_keyword('PROPERTIES'))
+        self.assertTrue(self.verify.is_first_class_keyword(Tokens.reindent(1) + 'PROPERTY'))
+        self.assertFalse(self.verify.is_first_class_keyword('PROPERTY2'))
+        self.assertFalse(self.verify.is_first_class_keyword('proPERTY'))
+
+    def test_available_properties_version(self):
+        self.assertEqual('3.17.0', self.verify.get_cmake_properties_version())
+
+    def test_cmake_properties_matching_exactly(self):
+        self.assertTrue(self.verify.is_property('LINK_DIRECTORIES'))
+        self.assertTrue(self.verify.is_property('INSTALL_REMOVE_ENVIRONMENT_RPATH'))
+        self.assertFalse(self.verify.is_property('1INSTALL_REMOVE_ENVIRONMENT_RPATH'))
+
+    def test_cmake_properties_starting_with(self):
+        self.assertTrue(self.verify.is_property('IMPORTED_NO_SONAME'))
+        self.assertTrue(self.verify.is_property('IMPORTED_NO_SONAME_123'))
+        self.assertFalse(self.verify.is_property('IMPORTED_NO_SONAME123'))
+        self.assertFalse(self.verify.is_property('123_IMPORTED_NO_SONAME_123'))
+        self.assertFalse(self.verify.is_property('IMPORTED_NO_SONAM'))

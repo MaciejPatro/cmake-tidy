aca06ef20f28e465b80cfaac0f4728efc3631028
Maciej Patro <maciejpatro@gmail.com>
2020-04-18 17:30:49 +0200

    Move formatting part of format_command_invocation to separate class

--- cmake_tidy/formatting/utils/format_command_invocation.py
+++ cmake_tidy/formatting/utils/format_command_invocation.py
@@ -4,11 +4,8 @@
 ###############################################################################
 
 
+from cmake_tidy.formatting.utils.invocation.command_formatter import CommandFormatter
 from cmake_tidy.formatting.utils.updaters.command_invocatin_state_updater import CommandInvocationStateUpdater
-from cmake_tidy.formatting.utils.invocation.invocation_splitter import InvocationSplitter
-from cmake_tidy.formatting.utils.invocation.invocation_wrapper import InvocationWrapper
-from cmake_tidy.formatting.utils.single_indent import get_single_indent
-from cmake_tidy.formatting.utils.tokens import Tokens
 
 
 class FormatCommandInvocation:
@@ -30,55 +27,4 @@ class FormatCommandInvocation:
                 'closing': data[2] if len(data) == 3 else data[1]}
 
     def __format_invocation(self, invocation: dict) -> str:
-        invocation['arguments'] = self.__prepare_arguments(invocation)
-        formatted = self.__join_command_invocation(invocation)
-        return self.__add_reindent_tokens_where_needed(formatted)
-
-    def __prepare_arguments(self, invocation: dict) -> list:
-        invocation['arguments'] = self.__remove_empty_arguments(invocation)
-        if self.__is_wrappable(invocation):
-            invocation['arguments'] = self.__wrap_arguments_if_possible(invocation)
-        if not self.__is_fitting_in_line(invocation):
-            invocation['arguments'] = self.__split_command_to_newlines(invocation)
-        return invocation['arguments']
-
-    @staticmethod
-    def __remove_empty_arguments(invocation: dict) -> list:
-        return list(filter(len, invocation['arguments']))
-
-    def __wrap_arguments_if_possible(self, invocation: dict) -> list:
-        command_invocation = InvocationWrapper().wrap(invocation)
-        if self.__is_fitting_in_line(command_invocation):
-            return command_invocation['arguments']
-        else:
-            return invocation['arguments']
-
-    def __is_fitting_in_line(self, command_invocation: dict) -> bool:
-        return self.__invocation_length(command_invocation) < self.__settings['line_length']
-
-    def __invocation_length(self, command_invocation: dict) -> int:
-        invoke = self.__join_command_invocation(command_invocation) + self.__newline_indent()
-        invoke = invoke.replace('\t', ' ' * self.__settings['tab_size'])
-        return len(invoke) - len(Tokens.reindent(1))
-
-    def __newline_indent(self) -> str:
-        indent = max(self.__state['indent'] - 1, 0)
-        return indent * get_single_indent(self.__settings)
-
-    def __is_wrappable(self, invocation: dict) -> bool:
-        return len(invocation['arguments']) > 0 and self.__settings['wrap_short_invocations_to_single_line'] is True
-
-    def __split_command_to_newlines(self, invocation: dict) -> list:
-        return InvocationSplitter(self.__state, self.__settings).split(invocation)
-
-    @staticmethod
-    def __join_command_invocation(invocation: dict) -> str:
-        formatted = invocation['function_name'] + ''.join(invocation['arguments']) + invocation['closing']
-        return formatted
-
-    @staticmethod
-    def __add_reindent_tokens_where_needed(data: str) -> str:
-        data_lower = data.lower()
-        if any(data_lower.startswith(token) for token in Tokens.reindent_commands_tokens()):
-            return Tokens.reindent(1) + data
-        return data
+        return CommandFormatter(self.__state, self.__settings).format_invocation(invocation)

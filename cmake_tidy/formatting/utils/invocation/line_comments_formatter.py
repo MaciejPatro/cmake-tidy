21ee00c5c7a3f5d3ceac4895f322e6e107bc8fe4
Maciej Patro <maciejpatro@gmail.com>
2020-05-06 18:10:24 +0200

    Line comments handling at the end of the command invocation now handled correctly

--- cmake_tidy/formatting/utils/invocation/line_comments_formatter.py
+++ cmake_tidy/formatting/utils/invocation/line_comments_formatter.py
@@ -12,12 +12,43 @@ from cmake_tidy.lexical_data import KeywordVerifier
 
 
 class LineCommentsFormatter:
-    def __init__(self, settings: dict):
+    def __init__(self, state: dict, settings: dict):
+        self.__state = state
         self.__settings = settings
 
     def format(self, args: List[str]) -> list:
         args = self.__reindent_line_comments(args)
         return self.__merge_line_comments_with_whitespaces_before(args)
+
+    def __reindent_line_comments(self, args: List[str]) -> list:
+        args = self.__reindent_line_comments_after_keyword(args)
+        return self.__reindent_line_comments_at_end_of_invocation(args)
+
+    def __reindent_line_comments_at_end_of_invocation(self, args: List[str]) -> list:
+        if self.__state['keyword_argument']:
+            self.__try_reindent_all_previous_comments(args, len(args))
+        return args
+
+    def __reindent_line_comments_after_keyword(self, args: List[str]) -> list:
+        verifier = KeywordVerifier(self.__settings)
+        for i in reversed(range(len(args))):
+            if verifier.is_keyword(args[i]) and re.match(Tokens.get_reindent_regex(), args[i]):
+                self.__try_reindent_all_previous_comments(args, i)
+        return args
+
+    def __try_reindent_all_previous_comments(self, args, i):
+        try:
+            self.__reindent_all_previous_comments(args, i)
+        except IndexError:
+            pass
+
+    @staticmethod
+    def __reindent_all_previous_comments(args: list, start: int) -> None:
+        for i in reversed(range(start)):
+            if Tokens.is_line_comment(args[i]):
+                args[i] = Tokens.reindent(1) + args[i]
+            else:
+                break
 
     @staticmethod
     def __merge_line_comments_with_whitespaces_before(args: List[str]) -> list:
@@ -28,25 +59,3 @@ class LineCommentsFormatter:
             else:
                 merged.append(args[i])
         return merged + [args[-1]] if args else []
-
-    def __reindent_line_comments(self, args: List[str]) -> list:
-        verifier = KeywordVerifier(self.__settings)
-
-        for i in reversed(range(len(args))):
-            if verifier.is_keyword(args[i]) and re.match(Tokens.get_reindent_regex(), args[i]):
-                self.__try_reindent_all_comments_after_keyword(args, i)
-        return args
-
-    def __try_reindent_all_comments_after_keyword(self, args, i):
-        try:
-            self.__reindent_all_comments_after_keyword(args, i)
-        except IndexError:
-            pass
-
-    @staticmethod
-    def __reindent_all_comments_after_keyword(args: list, start: int) -> None:
-        for i in reversed(range(start)):
-            if Tokens.is_line_comment(args[i]):
-                args[i] = Tokens.reindent(1) + args[i]
-            else:
-                break

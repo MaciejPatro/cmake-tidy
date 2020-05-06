71974815c24516ce657a3682b7cf4a26338b2996
Maciej Patro <maciejpatro@gmail.com>
2020-05-06 14:54:09 +0200

    Fix one of the unit tests for line comments, add unit tests for Tokens class ongoing

--- tests/unit/test_cmake_formatter_comments_within_arguments.py
+++ tests/unit/test_cmake_formatter_comments_within_arguments.py
@@ -12,16 +12,13 @@ from tests.unit.test_cmake_formatter import TestCMakeFormatter
 
 
 class TestCMakeFormatterCommandArgumentsWithComments(TestCMakeFormatter):
-    @skip
-    def test_multiple_line_comments_between_keywords(self):
+    def test_multiple_line_comments_before_value(self):
         args = arguments().add(unquoted_argument('abc')) \
             .add(spaces('    ')) \
             .add(unquoted_argument('TARGET')) \
             .add(newlines(1)) \
             .add(line_ending('# first line', 1)) \
             .add(line_ending('# second line', 1)) \
-            .add(unquoted_argument('TARGET')) \
-            .add(newlines(1)) \
             .add(unquoted_argument('${PROJECT_NAME}')) \
             .add(newlines(1))
 
@@ -29,6 +26,29 @@ class TestCMakeFormatterCommandArgumentsWithComments(TestCMakeFormatter):
 
         expected_formatting = """add_custom_target(abc
   TARGET
+    # first line
+    # second line
+    ${PROJECT_NAME}
+)"""
+        self.assertFormatting(expected_formatting, root)
+
+    @skip
+    def test_multiple_line_comments_before_keyword(self):
+        args = arguments().add(unquoted_argument('abc')) \
+            .add(newlines(1)) \
+            .add(unquoted_argument('ALL')) \
+            .add(newlines(1)) \
+            .add(line_ending('# first line', 1)) \
+            .add(line_ending('# second line', 1)) \
+            .add(unquoted_argument('TARGET')) \
+            .add(spaces('  ')) \
+            .add(unquoted_argument('${PROJECT_NAME}')) \
+            .add(newlines(1))
+
+        root = file().add(command_invocation('add_custom_target(', args))
+
+        expected_formatting = """add_custom_target(abc
+  ALL
   # first line
   # second line
   TARGET

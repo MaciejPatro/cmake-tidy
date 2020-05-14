dd42293d11f4334224880adbf03109eede7468ae
Maciej Patro <maciejpatro@gmail.com>
2020-05-14 16:20:15 +0200

    Fix #103 - newline handling at the end command invocation fixed

--- cmake_tidy/formatting/utils/invocation/command_splitter.py
+++ cmake_tidy/formatting/utils/invocation/command_splitter.py
@@ -21,7 +21,7 @@ class CommandSplitter:
     def split(self, invocation: dict) -> list:
         invocation['arguments'] = self.__split_args_to_newlines(invocation['arguments'])
         invocation['arguments'] = self.__realign(invocation)
-        return invocation['arguments'] + self.__add_closing_bracket_separator(invocation)
+        return invocation['arguments'] + self.__add_closing_parenthesis_separator(invocation)
 
     def __realign(self, invocation: dict) -> list:
         return CommandRealignModifier(self.__state, self.__settings).realign(invocation)
@@ -35,7 +35,7 @@ class CommandSplitter:
         self.__state_updater.update_state(arg)
         return self.__get_converted_whitespace() if arg == ' ' else arg
 
-    def __add_closing_bracket_separator(self, invocation: dict) -> list:
+    def __add_closing_parenthesis_separator(self, invocation: dict) -> list:
         if self.__settings['closing_parentheses_in_newline_when_split'] and \
                 not self.__is_last_element_newline(invocation):
             return [FormatNewline(self.__state, self.__settings)(1)]
@@ -43,7 +43,8 @@ class CommandSplitter:
 
     @staticmethod
     def __is_last_element_newline(invocation: dict) -> bool:
-        return Tokens.is_spacing_token(invocation['arguments'][-1])
+        return Tokens.is_spacing_token(invocation['arguments'][-1]) or \
+               Tokens.is_line_comment(invocation['arguments'][-1])
 
     def __get_converted_whitespace(self) -> str:
         return FormatNewline(self.__state, self.__settings)(1)

95c00ee375eadb788bdbe76d7c8f78aab53c7b4a
Maciej Patro <maciejpatro@gmail.com>
2020-06-02 16:57:59 +0200

    Provide path to input file to settings reader - currently unused

--- cmake_tidy/formatting/__init__.py
+++ cmake_tidy/formatting/__init__.py
@@ -4,9 +4,11 @@
 ###############################################################################
 
 
+from pathlib import Path
+
 from cmake_tidy.formatting.cmake_formatter import CMakeFormatter
 from cmake_tidy.formatting.settings_reader import SettingsReader
 
 
-def try_read_settings() -> dict:
-    return SettingsReader().try_loading_format_settings()
+def try_read_settings(filepath: Path) -> dict:
+    return SettingsReader().try_loading_format_settings(filepath)

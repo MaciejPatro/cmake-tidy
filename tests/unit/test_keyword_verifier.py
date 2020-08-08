754b72d569a7b9ee685757bda427382b0659aac0
Maciej Patro <maciejpatro@gmail.com>
2020-08-08 19:15:25 +0200

    Add missing case to unit tests

--- tests/unit/test_keyword_verifier.py
+++ tests/unit/test_keyword_verifier.py
@@ -82,6 +82,7 @@ class TestKeywordVerifier(unittest.TestCase):
     def test_recognition_of_conditional_invocation(self):
         self.assertTrue(KeywordVerifier.is_conditional_invocation('If('))
         self.assertTrue(KeywordVerifier.is_conditional_invocation('while('))
+        self.assertTrue(KeywordVerifier.is_conditional_invocation('while ('))
         self.assertFalse(KeywordVerifier.is_conditional_invocation('foreach ('))
         self.assertFalse(KeywordVerifier.is_conditional_invocation('if2('))
         self.assertFalse(KeywordVerifier.is_conditional_invocation('if*'))

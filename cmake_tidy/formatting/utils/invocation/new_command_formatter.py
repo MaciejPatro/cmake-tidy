b1b7d261ab447b91f7ad638e0be18ccbf5595875
Maciej Patro <maciejpatro@gmail.com>
2020-08-30 14:36:38 +0200

    Next unit test introduce

--- cmake_tidy/formatting/utils/invocation/new_command_formatter.py
+++ cmake_tidy/formatting/utils/invocation/new_command_formatter.py
@@ -4,11 +4,19 @@
 ###############################################################################
 
 
+from typing import List
+
+from cmake_tidy.formatting.utils import FormatNewline
+
+
 class NewCommandFormatter:
     def __init__(self, state: dict, settings: dict):
         self.__state = state
         self.__settings = settings
+        self.__newline_formatter = FormatNewline(state, settings)
 
-    @staticmethod
-    def format(invocation: dict) -> str:
-        return invocation['function_name'] + ''.join(invocation['arguments']) + invocation['closing']
+    def format(self, invocation: dict) -> str:
+        return invocation['function_name'] + self.handle_arguments(invocation['arguments']) + invocation['closing']
+
+    def handle_arguments(self, args: List[str]) -> str:
+        return self.__newline_formatter(1).join(args)
--- tests/unit/test_new_command_formatter.py
+++ tests/unit/test_new_command_formatter.py
@@ -2,26 +2,33 @@
 # Copyright Maciej Patro (maciej.patro@gmail.com)
 # MIT License
 ###############################################################################
-
-
+import re
 import unittest
 from typing import Iterable
 
 from cmake_tidy.formatting.utils.invocation.new_command_formatter import NewCommandFormatter
-
-
-def make_invocation(name: str, arguments: Iterable[str]) -> dict:
-    return {'function_name': name, 'arguments': arguments, 'closing': ')'}
+from cmake_tidy.formatting.utils.tokens import Tokens
 
 
 class TestNewCommandFormatter(unittest.TestCase):
     def setUp(self) -> None:
-        self.state = dict()
-        self.settings = dict()
+        self.state = {'indent': 1}
+        self.settings = {'line_length': 2, 'tabs_as_spaces': False, 'succeeding_newlines': 2}
         self.formatter = NewCommandFormatter(self.state, self.settings)
 
     def test_format_empty_invocation(self):
-        self.assertEqual('set()', self.formatter.format(make_invocation('set(', [])))
+        self.assertEqual('set()', self.__get_formatted('set(', []))
 
     def test_invocation_with_single_argument(self):
-        self.assertEqual('set(argument)', self.formatter.format(make_invocation('set(', ['argument'])))
+        self.assertEqual('set(argument)', self.__get_formatted('set(', ['argument']))
+
+    def test_invocation_with_two_arguments_split_due_to_line_length(self):
+        self.assertEqual('set(argument\n\tnext)', self.__get_formatted('set(', ['argument', 'next']))
+
+    @staticmethod
+    def __make_invocation(name: str, arguments: Iterable[str]) -> dict:
+        return {'function_name': name, 'arguments': arguments, 'closing': ')'}
+
+    def __get_formatted(self, name: str, arguments: Iterable[str]) -> str:
+        raw = self.formatter.format(self.__make_invocation(name, arguments))
+        return re.sub(Tokens.remove_spaces(), '', raw)
